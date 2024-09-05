af9cc6dfa1d7de2cfe7e876d041964b788c063ec
Richard Sullivan <richardsullivan@Richards-MBP.lan>
2024-09-05 14:42:46 -0500

    feat: create simple file server pointed at www dir

--- server.go
+++ server.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("starting server...")
+	log.Fatal(
+		http.ListenAndServe(":8080", http.FileServer(http.Dir("/var/www/Richard-M-Sullivan.github.io"))))
 }
