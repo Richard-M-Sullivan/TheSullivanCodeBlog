009c937734392172081aef5356d06d824c4e30cf
Richard Sullivan <richard.m.sullivan2@gmail.com>
2026-09-16 14:36:59 -0500

    feat: create pages for 404 and 500 errors

--- TheSullivanCodeBlog.js
+++ TheSullivanCodeBlog.js
@@ -40,16 +40,14 @@ app.use(function (req, res, next) {
 //custom 404 page
 app.use(function(req, res) {
   res.status(404)
-  //res.render('404');
-  res.send('404: Page not found.');
+  res.render('404');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500);
-  //res.render('500');
-  res.send('500: Internal server error.');
+  res.render('500');
 });
 
 // launch application
